2f9e85a60815527b0651b6ebf2039d630a861b03
Tang <1024830255@qq.com>
2018-12-30 16:59:57 +0800

    科举查询

---
+++ awesome/plugins/exam/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@Time:  2018/12/30 15:56
+@author:10248
+"""
+
+from none import on_command, CommandSession
+
+from awesome.plugins.exam.data_source import get_exam
+
+
+@on_command('exam', aliases=('科举', '科举查询'), shell_like=True)
+async def exam(session: CommandSession):
+    values = session.args['argv']
+    await session.send(await get_exam(values))
---
+++ awesome/plugins/exam/data_source.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+@Time:  2018/12/30 16:07
+@author:10248
+"""
+import json
+
+import ssl
+from urllib import parse, request
+
+
+async def get_exam(values):
+    q = ''
+    if len(values) >= 1:
+        q = values[0]
+    parametes = parse.urlencode({'m': 1,
+                                 'q': q
+                                 })
+    ssl._create_default_https_context = ssl._create_unverified_context
+    url = 'https://jx3.derzh.com/exam/?' + parametes
+    req = request.Request(url)
+    with request.urlopen(req) as data:
+        this_data = data.read().decode('utf-8')
+    json_dict = json.loads(this_data)
+    results = json_dict['result']
+    exam_str = '科举:'
+    for result in results:
+        exam_str += '\n\n问题:' + result['ques'] + \
+                    '\n答案:' + result['answ']
+    return exam_str
