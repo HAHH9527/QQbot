ba8956bc2a2e84528490edc158f6ea86a2c45fb1
Tang <1024830255@qq.com>
2018-12-30 15:58:28 +0800

    修改奇遇显示格式，添加数量限制

--- awesome/plugins/serendipity/data_source.py
+++ awesome/plugins/serendipity/data_source.py
@@ -8,21 +8,21 @@ import ssl
 import time
 from urllib import parse, request
 
-import prettytable
-from prettytable import PrettyTable
-
 
 async def get_serendipity(values):
-    S = s = n = ''
-    # 服务器
+    S = s = n = ''  # 服务器 奇遇名 角色名
+    serendipity_len = 5  # 数据条数
     if len(values) >= 1:
-        S = values[0]
-    # 奇遇名
+        if values[0] != '.':
+            S = values[0]
     if len(values) >= 2:
-        s = values[1]
-    # 角色名
+        if values[1] != '.':
+            s = values[1]
     if len(values) >= 3:
-        n = values[2]
+        if values[2] != '.':
+            n = values[2]
+    if len(values) >= 4:
+        serendipity_len = values[3]
     ssl._create_default_https_context = ssl._create_unverified_context
     values = parse.urlencode({'m': 1, 'test': 1,
                               'S': S,  # 服务器
@@ -36,11 +36,17 @@ async def get_serendipity(values):
     print(this_data)
     json_dict = json.loads(this_data)
     results = json_dict['result']
-    table = PrettyTable(["服务器", "名字", "奇遇", "时间"])
+    serendipity_str = '奇遇:'
+    flag_for = 0
     for result in results:
-        table.add_row([result['server'], result['name'], result['serendipity'], await time_format(result['time'])])
-        table.set_style(prettytable.PLAIN_COLUMNS)
-        return str(table)
+        serendipity_str += '\n\n' + result['name'] + ':' + \
+                           '\n|-' + result['server'] + \
+                           '\n|-' + result['serendipity'] + \
+                           '\n|-' + await time_format(result['time'])
+        flag_for = flag_for + 1
+        if flag_for >= int(serendipity_len):
+            break
+    return serendipity_str
 
 
 async def time_format(time_stamp):
